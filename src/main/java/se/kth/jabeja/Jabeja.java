b946a2cb96748371c6f1340fb1e8cb23828828e4
silverpas <silvia2.pasini@mail.polimi.it>
2025-12-08 10:02:10 +0100

    finito task1, task2 e parte opzionale

--- src/main/java/se/kth/jabeja/Jabeja.java
+++ src/main/java/se/kth/jabeja/Jabeja.java
@@ -13,15 +13,20 @@ import java.util.*;
 public class Jabeja {
   final static Logger logger = Logger.getLogger(Jabeja.class);
   private final Config config;
-  private final HashMap<Integer/*id*/, Node/*neighbors*/> entireGraph;
+  private final HashMap<Integer, Node> entireGraph;
   private final List<Integer> nodeIds;
   private int numberOfSwaps;
   private int round;
-  // private float T; for Task1
-  private double T; //Task2 required 
-
+  private double T;
+  
+  private double MAX_T;
+  private final double MIN_T = Math.pow(10, -5);
   private boolean resultFileCreated = false;
+  private int reset_rounds = 0;
+  private int exponent_round = 0;
 
+  private boolean task2 = true;
+  private final String optional = "LoriSilvia";
 
   //-------------------------------------------------------------------
   public Jabeja(HashMap<Integer, Node> graph, Config config) {
@@ -30,17 +35,23 @@ public class Jabeja {
     this.round = 0;
     this.numberOfSwaps = 0;
     this.config = config;
-    this.T = config.getTemperature();
+    if (this.task2) {
+      this.T = 1;
+      this.MAX_T = 1;
+      config.setDelta((float) 0.9);
+    }
+    else {
+     this.T = config.getTemperature();
+    }
   }
 
 
   //-------------------------------------------------------------------
- public void startJabeja() throws IOException {
+  public void startJabeja() throws IOException {
     for (round = 0; round < config.getRounds(); round++) {
       for (int id : entireGraph.keySet()) {
         sampleAndSwap(id);
       }
-
       //one cycle for all nodes have completed.
       //reduce the temperature
       saCoolDown();
@@ -48,286 +59,110 @@ public class Jabeja {
     }
   }
 
-  //start jabeja after 400
-
-  /*public void startJabeja() throws IOException {
-    for (round = 0; round < config.getRounds(); round++) {
-
-        // Restart SA at round 400
-        if (round == 400) {
-            T = config.getTemperature();
-            logger.info("=== RESTART SA at round " + round + ", T reset to " + T + " ===");
-        }
-
-        for (int id : entireGraph.keySet()) {
-            sampleAndSwap(id);
-        }
-
-        saCoolDown();
-        report();
+  /**
+   * Computing the acceptance probability
+   */
+  private double computeAcceptance(double new_val, double old_val){
+    if (optional.equals("LoriSilvia")) {
+      return Math.exp((new_val - old_val) / Math.pow(T, exponent_round));
     }
-
+    else {
+     return Math.exp((new_val - old_val) / T);
+    }
   }
-
-
 
   /**
-   * Simulated analealing cooling function
-   * TASK2 version
-   */private void saCoolDown(){
-    // Task 2: exponential cooling
-    double alpha = 1.0 - config.getDelta();   // es. delta=0.003 -> alpha=0.997
-
-    if (alpha < 0.0) {
-      alpha = 0.0;  // sicurezza, ma in pratica non dovrebbe succedere
+   * Simulated annealing cooling function
+   */
+  private void saCoolDown(){
+    if (task2){
+      exponent_round++;
+      T *= config.getDelta();
+      if (T < MIN_T) {
+        T = MIN_T;
+      }
+      if (T == MIN_T) {
+        reset_rounds++;
+          if (reset_rounds == 400) {
+            T = 1;
+            reset_rounds = 0;
+            exponent_round = 0;
+          }
+      }
     }
-
-    T = T * alpha;
-
-    // stesso lower bound di prima per coerenza con il Task 1
-    if (T < 1.0) {
-      T = 1.0;
+    else {
+      if (T > 1)
+        T -= config.getDelta();
+      if (T < 1)
+        T = 1;
     }
   }
-
-  /* TASK1 version
-  *private void saCoolDown(){
-    // TODO for second task
-    if (T > 1)
-      T -= config.getDelta();
-    if (T < 1)
-      T = 1;
-  }*/
 
   /**
    * Sample and swap algorith at node p
    * @param nodeId
-   * Try to find a swap partner for this node and swap colors.
-   * The sampling depends on the node selection policy:
-   * LOCAL  -> sample neighbors only
-   * RANDOM -> sample random nodes in the whole graph
-   * HYBRID -> first neighbors, then random nodes if needed
    */
   private void sampleAndSwap(int nodeId) {
-    Node nodep = entireGraph.get(nodeId);
     Node partner = null;
+    Node nodep = entireGraph.get(nodeId);
 
-    // First, try to find a partner among neighbors (LOCAL or HYBRID)
+    if (config.getNodeSelectionPolicy() == NodeSelectionPolicy.HYBRID || config.getNodeSelectionPolicy() == NodeSelectionPolicy.LOCAL) {
+      //swap with random neighbors
+        partner = findPartner(nodeId, getNeighbors(nodep));
+    }
+
     if (config.getNodeSelectionPolicy() == NodeSelectionPolicy.HYBRID
-        || config.getNodeSelectionPolicy() == NodeSelectionPolicy.LOCAL) {
-
-      Integer[] neighbors = getNeighbors(nodep);
-      partner = findPartner(nodeId, neighbors);
+            || config.getNodeSelectionPolicy() == NodeSelectionPolicy.RANDOM) {
+      // if local policy fails then randomly sample the entire graph
+      if (partner == null) {
+        partner = findPartner(nodeId, getSample(nodeId));
+      }
     }
 
-    // If no partner was found, try random nodes in the whole graph (RANDOM or HYBRID)
-    if (partner == null &&
-        (config.getNodeSelectionPolicy() == NodeSelectionPolicy.HYBRID
-         || config.getNodeSelectionPolicy() == NodeSelectionPolicy.RANDOM)) {
-
-      Integer[] randomSample = getSample(nodeId);
-      partner = findPartner(nodeId, randomSample);
-    }
-
-    // If a good partner exists, swap the colors
+    // swap the colors
     if (partner != null) {
-      int colorP = nodep.getColor();
-      int colorQ = partner.getColor();
-
-      nodep.setColor(colorQ);
-      partner.setColor(colorP);
-
-      numberOfSwaps++;
+        int aux = partner.getColor();
+        partner.setColor(nodep.getColor());
+        nodep.setColor(aux);
+        numberOfSwaps++;
     }
   }
 
-  // find partner optional task
   public Node findPartner(int nodeId, Integer[] nodes){
 
     Node nodep = entireGraph.get(nodeId);
-    int colorP = nodep.getColor();
 
     Node bestPartner = null;
-    double bestNewValue = Double.NEGATIVE_INFINITY;
+    double highestBenefit = 0;
 
-    double alpha = config.getAlpha();
-
-    if (nodes == null) return null;
-
-    for (Integer candidateId : nodes) {
-
-        Node nodeq = entireGraph.get(candidateId);
-        if (nodeq == null) continue;
-
-        int colorQ = nodeq.getColor();
-        if (colorP == colorQ) continue;
-
-        // Gradi attuali
-        int d_pp = getDegree(nodep, colorP);
-        int d_qq = getDegree(nodeq, colorQ);
-
-        // Gradi se scambiassimo i colori
-        int d_pq = getDegree(nodep, colorQ);
-        int d_qp = getDegree(nodeq, colorP);
-
-        // Valore prima e dopo
-        double oldValue = Math.pow(d_pp, alpha) + Math.pow(d_qq, alpha);
-        double newValue = Math.pow(d_pq, alpha) + Math.pow(d_qp, alpha);
-
-        double delta = newValue - oldValue;
-
-        // Acceptance probability: sigmoid
-        double prob = 1.0 / (1.0 + Math.exp(-(delta / T)));
-
-        // Random number
-        double rand = RandNoGenerator.nextInt(1000000) / 1000000.0;
-
-        boolean accept = rand < prob;
-
-        // Mantieni il partner migliore tra quelli accettati
-        if (accept && newValue > bestNewValue) {
-            bestNewValue = newValue;
-            bestPartner = nodeq;
-        }
-    }
-
-    return bestPartner;
-}
-
-
-  /* 
-   * Find the best partner for a color swap among a set of candidate nodes.
-   * A partner is accepted only if the swap improves the local objective,
-   * according to the JA-BE-JA formula and the current temperature T.
-   * TASK2 version
-   *
-    public Node findPartner(int nodeId, Integer[] nodes){
-
-    Node nodep = entireGraph.get(nodeId);
-    int colorP = nodep.getColor();
-
-    Node bestPartner = null;
-    double bestNewValue = 0.0;
-
-    double alpha = config.getAlpha();
-
-    if (nodes == null) {
-      return null;
-    }
-
-    for (Integer candidateId : nodes) {
-      if (candidateId == null) {
-        continue;
-      }
-
-      Node nodeq = entireGraph.get(candidateId);
-      if (nodeq == null) {
-        continue;
-      }
-
-      int colorQ = nodeq.getColor();
-
-      // se hanno lo stesso colore, swap inutile
-      if (colorP == colorQ) {
-        continue;
-      }
-
-      // gradi con colori attuali
-      int d_pp = getDegree(nodep, colorP);
-      int d_qq = getDegree(nodeq, colorQ);
-
-      // gradi se swappassimo i colori
-      int d_pq = getDegree(nodep, colorQ);
-      int d_qp = getDegree(nodeq, colorP);
-
-      // valore prima dello swap
-      double oldValue = Math.pow(d_pp, alpha) + Math.pow(d_qq, alpha);
-      // valore dopo lo swap
-      double newValue = Math.pow(d_pq, alpha) + Math.pow(d_qp, alpha);
-
-      double delta = newValue - oldValue;
-      boolean accept = false;
-
-      if (delta > 0) {
-        // miglioramento: accetto sempre
-        accept = true;
-      } else {
-        // peggioramento: accetto con probabilità exp(delta / T)
-        double prob = Math.exp(delta / T);      // delta <= 0 => 0 < prob <= 1
-        double rand = RandNoGenerator.nextInt(1000000) / 1000000.0;
-        if (rand < prob) {
-          accept = true;
+    for(Integer q: nodes) {
+      Node nodeq = entireGraph.get(q);
+      int degree_pp = getDegree(nodep, nodep.getColor());
+      int degree_qq = getDegree(nodeq, nodeq.getColor());
+      double old_d = Math.pow(degree_pp, config.getAlpha()) + Math.pow(degree_qq, config.getAlpha());
+      int degree_pq = getDegree(nodep, nodeq.getColor());
+      int degree_qp = getDegree(nodeq, nodep.getColor());
+      double new_d = Math.pow(degree_pq, config.getAlpha()) + Math.pow(degree_qp, config.getAlpha());
+      if (task2) {
+        Random random = new Random();
+        double prob = random.nextDouble();
+        double acceptance = computeAcceptance(new_d, old_d);
+        // Check this in order to avoid having a 100% acceptance rate (convergence)
+        if (new_d != old_d  && acceptance > prob && acceptance > highestBenefit) {
+          bestPartner = nodeq;
+          highestBenefit = acceptance;
         }
       }
-
-      // tra tutti quelli accettati, prendo quello col valore nuovo più alto
-      if (accept && newValue > bestNewValue) {
-        bestNewValue = newValue;
+      else {
+        if(new_d * T > old_d && new_d > highestBenefit) {
         bestPartner = nodeq;
+        highestBenefit = new_d;
+        }
       }
     }
 
     return bestPartner;
-  }*/
-
-
-  /**
-   * TASK1 version
-  * public Node findPartner(int nodeId, Integer[] nodes){
-
-    Node nodep = entireGraph.get(nodeId);
-    int colorP = nodep.getColor();
-
-    Node bestPartner = null;
-    double bestNewValue = 0.0;
-
-    double alpha = config.getAlpha();
-
-    if (nodes == null) {
-      return null;
-    }
-
-    // Check each candidate node as a possible swap partner
-    for (Integer candidateId : nodes) {
-      if (candidateId == null) {
-        continue;
-      }
-
-      Node nodeq = entireGraph.get(candidateId);
-      if (nodeq == null) {
-        continue;
-      }
-
-      int colorQ = nodeq.getColor();
-
-      // Swapping with a node of the same color has no effect
-      if (colorP == colorQ) {
-        continue;
-      }
-
-      // Compute degrees for current colors and for swapped colors
-      int d_pp = getDegree(nodep, colorP);
-      int d_qq = getDegree(nodeq, colorQ);
-      int d_pq = getDegree(nodep, colorQ);
-      int d_qp = getDegree(nodeq, colorP);
-
-      // Old score (before swap)
-      double oldValue = Math.pow(d_pp, alpha) + Math.pow(d_qq, alpha);
-
-      // New score (after swap)
-      double newValue = Math.pow(d_pq, alpha) + Math.pow(d_qp, alpha);
-
-      // Accept this partner if new * T > old (simulated annealing rule)
-      // and keep the best one (highest newValue)
-      if (newValue * T > oldValue && newValue > bestNewValue) {
-        bestNewValue = newValue;
-        bestPartner = nodeq;
-      }
-    }
-
-    return bestPartner;
-  }*/
-  
+  }
 
   /**
    * The the degreee on the node based on color
